7c4f1191eca971f13409f9c08c3c883fa0f95021
Amanda <80652875+Amanda641@users.noreply.github.com>
2021-05-07 19:01:11 -0300

    Update test.py

--- test.py
+++ test.py
@@ -28,16 +28,16 @@ def test_salvar_score2():
 def test_efeitos_sonoros():
     tipo = "Game_Over"
     game_over = pygame.mixer.Sound(CAMINHO_DA_MUSICA_GAMEOVER)
-    assert game_over.play(tipo)
+    assert game_over.play()
 
 
 def test_efeitos_sonoros1():
     tipo = "Retorno"
     retorno = pygame.mixer.Sound(CAMINHO_DA_MUSICA_RETORNO)
-    assert retorno.play(tipo)
+    assert retorno.play()
 
 
 def test_efeitos_sonoros2():
     tipo = "Click"
     click = pygame.mixer.Sound(CAMINHO_DA_MUSICA_SELECIONAR)
-    assert click.play(tipo)
+    assert click.play()
